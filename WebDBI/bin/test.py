e88a8c2d4a5d4f99efbef78a08af4434a04c48b1
David Caratelli <davidc1@uboonegpvm06.fnal.gov>
2015-02-11 09:42:26 -0600

    Revert "Takeout implementation from header... now works w/ dict at least on one set of compiler... --kazu" Revert to previous version of Snapshot This reverts commit d98ebfccfd93188eb8f8f38ac7c0d82426116f01.

--- WebDBI/bin/test.py
+++ WebDBI/bin/test.py
@@ -8,7 +8,7 @@ ts.SetSec(12347578)
 data = db.Request("pedestals",ts);
 print data.NChannels()
 for x in xrange(data.NChannels()):
-    ch=data.ChData(x)
+    ch=data.Data(x)
     print "channel:",ch.Channel()
     for y in xrange(ch.size()):
         print ch[y],
