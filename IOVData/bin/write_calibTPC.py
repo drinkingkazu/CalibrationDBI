c817b7b0738f48d7a9e33288d6c831ece599727c
David Caratelli <davidc1@uboonegpvm06.fnal.gov>
2015-02-11 14:04:54 -0600

    Modifications to GetCalibTPCResults --DC

--- IOVData/bin/write_calibTPC.py
+++ IOVData/bin/write_calibTPC.py
@@ -4,12 +4,17 @@ import sys, os
 if (len(sys.argv) != 2):
     print "Please provide a single input file as argument."
 
+ff = TFile.Open("out.root","RECREATE")
+
 calibTPCwriter = lariov.GetCalibTPCResults(sys.argv[1])
 calibTPCwriter.PrepareTrees()
+calibTPCwriter.PrepareSnapshotCollection("TPC_Calib_ASIC4.7_Sh1")
 calibTPCwriter.FillSnapshot(4.7,1.0)
+calibTPCwriter.PrepareSnapshotCollection("TPC_Calib_ASIC14_Sh1")
 calibTPCwriter.FillSnapshot(14,1.0)
+calibTPCwriter.PrepareSnapshotCollection("TPC_Calib_ASIC25_Sh1")
 calibTPCwriter.FillSnapshot(25,1.0)
-fout = TFile.Open("out.root","RECREATE")
-tree = TTree("tree_out","")
-calibTPCwriter.WriteSnapshotColl(fout,tree)
+calibTPCwriter.WriteToFile(ff)
+
+
 
